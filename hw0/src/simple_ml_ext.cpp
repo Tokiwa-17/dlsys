77970cab222b3f45c3f1b199a78137d25b6b25bb
Tokiwa-17 <2872444509@qq.com>
2023-05-30 16:52:40 +0800

    hw0 done

--- hw0/src/simple_ml_ext.cpp
+++ hw0/src/simple_ml_ext.cpp
@@ -6,49 +6,50 @@
 
 namespace py = pybind11;
 
-float *multiply(const float *a, const float *b, int a_height, int a_width, int b_height, int b_width) {
-    if (a_width != b_height) return nullptr;
-    float *c = new float(a_height * b_width);
-    for (int i = 0; i < a_height; i++)
-        for (int j = 0; j < b_height; j++) {
-            c[i * b_width + j] = 0.0f;
-            for (int k = 0; k < a_width; k++) {
-                c[i * b_width + j] += a[i * a_width + k] * b[k * b_width + j];
+float* mmproductFloat(const float *A, const float *B, size_t m, size_t n, size_t k) {
+    float* result = (float*)malloc(sizeof(float)*m*k);
+    memset(result, 0, sizeof(float)*m*k);
+    for(size_t i = 0; i < m; i++) { // first dim of norm_z
+        for(size_t j = 0; j < k; j++) { // second dim of norm_z
+            for(size_t ni = 0; ni < n; ni++) {
+                result[i*k + j] += A[i*n + ni] * B[ni*k + j];
             }
         }
-    return c;
+    }
+    return result;
 }
-
-float *exp(const float *a, int a_height, int a_width) {
-    auto b = new float(a_height * a_width);
-    for (int i = 0; i < a_height; i++)
-        for (int j = 0; j < a_width; j++) {
-            b[i * a_width + j] = std::expf(a[i * a_width + j]);
+float *exp(const float *a, size_t a_height, size_t a_width) {
+    float *b = (float*)malloc(sizeof(float) * a_height * a_width);
+    memset(b, 0, sizeof(float) * a_height * a_width);
+    for (size_t i = 0; i < a_height; i++)
+        for (size_t j = 0; j < a_width; j++) {
+            b[i * a_width + j] = exp(a[i * a_width + j]);
         }
     return b;
 }
 
-float *normalize(const float *a, int a_height, int a_width) {
-    auto b = new float(a_height * a_width);
-    std::vector<float> _sum;
-    for (int i = 0; i < a_height; i++) {
+float *normalize(const float *a, size_t a_height, size_t a_width) {
+    float *b = (float*)malloc(sizeof(float) * a_height * a_width);
+    float *_sum = (float*)malloc(sizeof(float) * a_height);
+    for (size_t i = 0; i < a_height; i++) {
         float row_sum = 0.0f;
-        for (int j = 0; j < a_width; j++) {
+        for (size_t j = 0; j < a_width; j++) {
             row_sum += a[i * a_width + j];
             b[i * a_width + j] = a[i * a_width + j];
         }
-        _sum.emplace_back(row_sum);
+        _sum[i] = row_sum;
     }
-    for (int i = 0; i < a_height; i++) {
-        for (int j = 0; j < a_width; j++) {
+    for (size_t i = 0; i < a_height; i++) {
+        for (size_t j = 0; j < a_width; j++) {
             b[i * a_width + j] /= _sum[i];
         }
     }
+    std::free(_sum);
     return b;
 }
 
 float * generateIy(const unsigned char *y, int l, int r, int height, int width) {
-    float * Iy = new float(height * width);
+    float * Iy = (float*)malloc(sizeof(float) * height * width);
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             Iy[i * width + j] = 0.0f;
@@ -70,7 +71,7 @@ void subtract(float *a, float *b, int height, int width) {
 }
 
 float * transpose(const float *a, int height, int width) {
-    float *b = new float[height * width];
+    float *b = (float*)malloc(sizeof(float) * height * width);
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             b[j * height + i] = a[i * width + j];
@@ -88,8 +89,8 @@ void elementMultiply(float *a, float val, int height, int width) {
 }
 
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
-								  float *theta, size_t m, size_t n, size_t k,
-								  float lr, size_t batch)
+                                  float *theta, size_t m, size_t n, size_t k,
+                                  float lr, size_t batch)
 {
     /**
      * A C++ version of the softmax regression epoch code.  This should run a
@@ -114,33 +115,34 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      */
 
     /// BEGIN YOUR CODE
+    // np.subtract(theta,  lr * (1 / batch) * X[l:r, :].T @ (normalize(np.exp(X[l:r, :] @ theta)) - Iy), out=theta)
     int iter = std::ceil(m / batch);
     for (int i = 0; i < iter; i++) {
-        auto l = i * batch, r = (i + 1) * batch >= m ? m : (i + 1) * batch;
+        int l = i * batch, r = (i + 1) * batch >= m ? m : (i + 1) * batch;
         // X[l:r, :] @ theta
-        auto tmp1 = multiply(X + l * n, theta, r - l + 1, n, n, k);
+        float* tmp1 = mmproductFloat(X + l * n, theta, r - l , n, k);
         // np.exp(X[l:r, :] @ theta
-        auto tmp2 = exp(tmp1, r - l + 1, k);
+        float *tmp2 = exp(tmp1, r - l, k);
         // normalize(np.exp(X[l:r, :] @ theta)
-        auto tmp3 = normalize(tmp2, r - l + 1, k);
+        float *tmp3 = normalize(tmp2, r - l, k);
         // Iy = np.eye(k)[y[l:r]]
-        auto Iy = generateIy(y, l, r, r - l + 1, k);
+        float *Iy = generateIy(y, l, r, r - l, k);
         // (normalize(np.exp(X[l:r, :] @ theta) - Iy)
-        subtract(tmp3, Iy, r - l + 1, k);
+        subtract(tmp3, Iy, r - l, k);
         // X[l:r, :].T
-        auto tmp4 = transpose(X + l * n, r - l + 1, n);
+        float *tmp4 = transpose(X + l * n, r - l, n);
         // X[l:r, :].T @ (normalize(np.exp(X[l:r, :] @ theta)) - Iy)
-        auto tmp5 = multiply(tmp4, tmp3, n, r - l + 1, r - l + 1, k);
+        float *tmp5 = mmproductFloat(tmp4, tmp3, n, r - l, k);
         //  lr * (1 / batch) * X[l:r, :].T @ (normalize(np.exp(X[l:r, :] @ theta)) - Iy)
-        elementMultiply(tmp5, lr * 1 / (r - l + 1), n, k);
+        elementMultiply(tmp5, lr * 1.0f / (r - l), n, k);
         // result
         subtract(theta, tmp5, n, k);
-        delete[] tmp1;
-        delete[] tmp2;
-        delete[] tmp3;
-        delete[] tmp4;
-        delete[] tmp5;
-        delete[] Iy;
+        std::free(tmp1);
+        std::free(tmp2);
+        std::free(tmp3);
+        std::free(tmp4);
+        std::free(tmp5);
+        std::free(Iy);
     }
     /// END YOUR CODE
 }
@@ -152,23 +154,23 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
  * edits to the code
  */
 PYBIND11_MODULE(simple_ml_ext, m) {
-    m.def("softmax_regression_epoch_cpp",
-    	[](py::array_t<float, py::array::c_style> X,
-           py::array_t<unsigned char, py::array::c_style> y,
-           py::array_t<float, py::array::c_style> theta,
-           float lr,
-           int batch) {
-        softmax_regression_epoch_cpp(
-        	static_cast<const float*>(X.request().ptr),
-            static_cast<const unsigned char*>(y.request().ptr),
-            static_cast<float*>(theta.request().ptr),
-            X.request().shape[0],
-            X.request().shape[1],
-            theta.request().shape[1],
-            lr,
-            batch
-           );
-    },
-    py::arg("X"), py::arg("y"), py::arg("theta"),
-    py::arg("lr"), py::arg("batch"));
+m.def("softmax_regression_epoch_cpp",
+[](py::array_t<float, py::array::c_style> X,
+        py::array_t<unsigned char, py::array::c_style> y,
+py::array_t<float, py::array::c_style> theta,
+float lr,
+int batch) {
+softmax_regression_epoch_cpp(
+static_cast<const float*>(X.request().ptr),
+static_cast<const unsigned char*>(y.request().ptr),
+static_cast<float*>(theta.request().ptr),
+X.request().shape[0],
+X.request().shape[1],
+theta.request().shape[1],
+lr,
+batch
+);
+},
+py::arg("X"), py::arg("y"), py::arg("theta"),
+py::arg("lr"), py::arg("batch"));
 }
