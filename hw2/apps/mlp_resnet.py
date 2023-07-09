244c17e8289f9743789edd7b27f9be8df550e4fb
Tokiwa-17 <2872444509@qq.com>
2023-07-09 15:55:18 +0800

    ResidualBlock && MLPResNet

--- hw2/apps/mlp_resnet.py
+++ hw2/apps/mlp_resnet.py
@@ -5,18 +5,29 @@ import needle.nn as nn
 import numpy as np
 import time
 import os
+from pathlib import Path
 
 np.random.seed(0)
 
 def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    modules = []
+    fn = nn.Sequential(nn.Linear(dim, hidden_dim), norm(hidden_dim), nn.ReLU(), nn.Dropout(drop_prob), \
+                       nn.Linear(hidden_dim, dim), norm(dim))
+    modules.append(nn.Residual(fn))
+    modules.append(nn.ReLU())
+    return nn.Sequential(*modules)
     ### END YOUR SOLUTION
 
 
 def MLPResNet(dim, hidden_dim=100, num_blocks=3, num_classes=10, norm=nn.BatchNorm1d, drop_prob=0.1):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    modules = nn.Sequential(
+        nn.Linear(dim, hidden_dim),
+        *[ResidualBlock(hidden_dim, hidden_dim // 2, norm, drop_prob) for _ in range(num_blocks)],
+        nn.Linear(hidden_dim, dim),
+    )
+    return nn.Sequential(modules)
     ### END YOUR SOLUTION
 
 
@@ -25,7 +36,12 @@ def MLPResNet(dim, hidden_dim=100, num_blocks=3, num_classes=10, norm=nn.BatchNo
 def epoch(dataloader, model, opt=None):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    if opt == None: model.eval()
+    for idx, batch in enumerate(dataloader):
+        batch_size = batch[0].shape[0]
+        batch_x, batch_y = batch[0].reshape((batch_size, -1)), batch[1]
+        pred = model(batch_x)
+        print('test')
     ### END YOUR SOLUTION
 
 
@@ -34,7 +50,22 @@ def train_mnist(batch_size=100, epochs=10, optimizer=ndl.optim.Adam,
                 lr=0.001, weight_decay=0.001, hidden_dim=100, data_dir="data"):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    dim = 28 ** 2
+    model = MLPResNet(dim, hidden_dim)
+    data_path = Path(data_dir)
+    mnist_train_dataset = ndl.data.MNISTDataset(data_path / "train-images-idx3-ubyte.gz",
+                                                data_path / "train-labels-idx1-ubyte.gz")
+    mnist_train_dataloader = ndl.data.DataLoader(dataset=mnist_train_dataset,
+                                                 batch_size=batch_size,
+                                                 shuffle=False)
+    mnist_test_dataset = ndl.data.MNISTDataset(data_path / "t10k-images-idx3-ubyte.gz",
+                                               data_path / "t10k-labels-idx1-ubyte.gz")
+    mnist_test_dataloader = ndl.data.DataLoader(dataset=mnist_test_dataset,
+                                                batch_size=batch_size,
+                                                shuffle=False)
+    for _ in range(epochs):
+        epoch(mnist_train_dataloader, model, optimizer)
+        #epoch(mnist_test_dataloader, )
     ### END YOUR SOLUTION
 
 
